5d5d5b1c427ddc4de8141c8b435c8e0a01031760
Gabriel Vander <gabsvander@hotmail.com>
2022-11-11 23:41:16 -0300

    Adjustments to MainMenu

--- src/core/presentation/main_menu.py
+++ src/core/presentation/main_menu.py
@@ -1,26 +1,76 @@
 from __future__ import annotations
 
-from src.core.presentation.utils.options_menu import Option, OptionsMenu
-from src.core.presentation.utils.user_input_menu import UserInputMenu
+import abc
+import dataclasses
+import typing
+from collections.abc import Awaitable, Callable, Generator
+
+from src.core.utils.result import Ok, Result
+from src.features.price_provisioning.price_provisioning_controller import (
+    PriceProvisioningController,
+    PriceProvisioningControllerFailure, ProvisionedPricesViewModel,
+)
 
 
 class MainMenu:
+    _options: list[_Option]
+    _price_provisioning_controller: PriceProvisioningController
+
+    def __init__(self, price_provisioning_controller: PriceProvisioningController) -> None:
+        self._price_provisioning_controller = price_provisioning_controller
+        self._options = [
+            _AsyncOption(id=1, display_message='Display raw data', on_select=self._display_raw_data),
+        ]
+
+    async def run(self) -> None:
+        self._display_header()
+        self._display_options()
+        await self._get_and_run_user_choice()
+
+    def _display_options(self):
+        options: list[_Option] = self._options
+        options.sort(key=lambda o: o.id)
+
+        for opt in options:
+            print(f'{opt.id} - {opt.display_message}')
+
+    async def _get_and_run_user_choice(self):
+        available_options: list[int] = list(map(lambda o: o.id, self._options))
+        maximum_attempts: int = 20
+
+        for _ in range(maximum_attempts):
+            result: str = input('\nChoose an option...\n')
+
+            if result.isnumeric() and (choice_id := int(result)) in available_options:
+                await self._select_option_with_id(choice_id)
+                return
+
+    async def _select_option_with_id(self, choice_id: int) -> None:
+        filtered_options: list[_Option] = list(filter(lambda o: o.id == choice_id, self._options))
+
+        if selected_option := filtered_options[0]:
+            if isinstance(selected_option, _AsyncOption):
+                async_option: _AsyncOption = typing.cast(_AsyncOption, selected_option)
+                await async_option.on_select()
+
+    async def _display_raw_data(self) -> None:
+        raw_prices_result: Result[ProvisionedPricesViewModel, PriceProvisioningControllerFailure] = \
+            await self._price_provisioning_controller.provision_prices()
+
+        if raw_prices_result.is_ok():
+            raw_prices_ok_result: Ok[ProvisionedPricesViewModel, PriceProvisioningControllerFailure] = typing.cast(
+                Ok, raw_prices_result
+            )
+
+            self._display_prices(raw_prices_ok_result.value)
 
     @staticmethod
-    async def run() -> None:
+    def _display_header() -> None:
         _Header(100).display()
-        print(
-            OptionsMenu(
-                options=[
-                    Option(display_message='Average Price Per Country')
-                ]
-            )
-            .display()
-            .ljust(100, ' ')
-        )
-        await UserInputMenu(
-            display_message='\nChoose an option...\n',
-        ).display_until_input()
+
+    @staticmethod
+    def _display_prices(view_model: ProvisionedPricesViewModel) -> None:
+        _RawDataDisplayer(view_model=view_model).run()
 
 
 class _Header:
@@ -35,3 +85,45 @@ class _Header:
         text: str = '\n'.join(self._lines)
 
         print(text)
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class _Option(abc.ABC):
+    id: int
+    display_message: str
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class _SyncOption(_Option):
+    on_select: Callable[[], None]
+
+
+@dataclasses.dataclass(frozen=True, kw_only=True)
+class _AsyncOption(_Option):
+    on_select: Callable[[], Awaitable[None]]
+
+
+class _RawDataDisplayer:
+    _view_model: ProvisionedPricesViewModel
+
+    def __init__(self, view_model: ProvisionedPricesViewModel) -> None:
+        self._view_model = view_model
+
+    def run(self) -> None:
+        lines: list[str] = list(self._generate_lines())
+
+        print('\n'.join(lines))
+
+    def _generate_lines(self) -> Generator[str, None, None]:
+        separator: str = '-' * 15
+
+        for i, price in enumerate(self._view_model.prices):
+            yield separator
+            yield f'Entry #{i + 1}:'
+            yield f'Country Name: {price.country_name}'
+            yield f'Prince in USD: {price.price_in_dollars}'
+            yield f'Local Currency: {price.local_currency_code}'
+            yield f'Price in local currency: {price.price_in_local_currency}'
+            yield f'USD exchange rate: {price.dollar_exchange_rate}'
+            yield f'Date: {price.date}'
+            yield separator
--- tests/unit_tests/core/presentation/test_main_menu.py
+++ tests/unit_tests/core/presentation/test_main_menu.py
@@ -8,15 +8,26 @@ import pytest
 from _pytest.monkeypatch import MonkeyPatch
 
 from src.core.presentation.main_menu import MainMenu
+from src.core.utils.result import Result
+from src.features.price_provisioning.price_provisioning_controller import (
+    PriceProvisioningController,
+    PriceViewModel, ProvisionedPricesViewModel,
+)
 
 
 class TestMainMenu:
     _decoy: decoy.Decoy
+    _dummy_price_provisioning_controller: PriceProvisioningController
+    _menu: MainMenu
 
     @pytest.fixture(autouse=True)
     def set_up_and_tear_down(self) -> Generator[None, None, None]:
         # Set Up
         self._decoy = decoy.Decoy()
+        self._dummy_price_provisioning_controller = self._decoy.mock(cls=PriceProvisioningController)
+        self._menu = MainMenu(
+            price_provisioning_controller=self._dummy_price_provisioning_controller,
+        )
 
         yield
 
@@ -28,13 +39,11 @@ class TestMainMenu:
         menu_width: int = 100
         expected_lines: list[str] = [
             'Big Mac Prices'.center(menu_width, '-'),
-            '1 - Average Price Per Country'.ljust(menu_width, ' ') + '\n',
-            'Choose an option...\n'
+            '1 - Display raw data\n',
         ]
         expected_text: str = '\n'.join(expected_lines)
         dummy_input: Callable[[str], str] = self._decoy.mock(func=Callable[[str], str])  # type: ignore
         mock_stdout: io.StringIO = io.StringIO()
-        menu: MainMenu = MainMenu()
 
         self._decoy.when(
             dummy_input('\nChoose an option...\n')
@@ -43,6 +52,122 @@ class TestMainMenu:
         monkeypatch.setattr(builtins, 'input', dummy_input)
         monkeypatch.setattr(sys, 'stdout', mock_stdout)
 
-        await menu.run()
+        await self._menu.run()
 
         assert mock_stdout.getvalue() == expected_text
+
+    # noinspection SpellCheckingInspection
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize(
+        'prices, expected_lines',
+        [
+            (ProvisionedPricesViewModel(prices=[]), []),
+            (ProvisionedPricesViewModel(
+                prices=[
+                    PriceViewModel(
+                        date='',
+                        price_in_dollars='',
+                        price_in_local_currency='',
+                        country_name='',
+                        dollar_exchange_rate='',
+                        local_currency_code='',
+                    ),
+                ]
+            ), [
+                 '-' * 15,
+                 'Entry #1:',
+                 'Country Name: ',
+                 'Prince in USD: ',
+                 'Local Currency: ',
+                 'Price in local currency: ',
+                 'USD exchange rate: ',
+                 'Date: ',
+                 '-' * 15,
+             ]),
+            (ProvisionedPricesViewModel(
+                prices=[
+                    PriceViewModel(
+                        date='S6Tzngi',
+                        price_in_dollars='u4H',
+                        price_in_local_currency='8M6fM8',
+                        country_name='5N2u86',
+                        dollar_exchange_rate='2J9w',
+                        local_currency_code='u1xc',
+                    ),
+                ]
+            ), [
+                 '-' * 15,
+                 'Entry #1:',
+                 'Country Name: 5N2u86',
+                 'Prince in USD: u4H',
+                 'Local Currency: u1xc',
+                 'Price in local currency: 8M6fM8',
+                 'USD exchange rate: 2J9w',
+                 'Date: S6Tzngi',
+                 '-' * 15,
+             ]),
+            (ProvisionedPricesViewModel(
+                prices=[
+                    PriceViewModel(
+                        date='S6Tzngi',
+                        price_in_dollars='u4H',
+                        price_in_local_currency='8M6fM8',
+                        country_name='5N2u86',
+                        dollar_exchange_rate='2J9w',
+                        local_currency_code='u1xc',
+                    ),
+                    PriceViewModel(
+                        date='SKj43n7b',
+                        price_in_dollars='K4ZSJ0Q',
+                        price_in_local_currency='hL1Wa',
+                        country_name='BPr',
+                        dollar_exchange_rate='W9Uw5Ul',
+                        local_currency_code='0p88',
+                    ),
+                ]
+            ), [
+                 '-' * 15,
+                 'Entry #1:',
+                 'Country Name: 5N2u86',
+                 'Prince in USD: u4H',
+                 'Local Currency: u1xc',
+                 'Price in local currency: 8M6fM8',
+                 'USD exchange rate: 2J9w',
+                 'Date: S6Tzngi',
+                 '-' * 15,
+                 '-' * 15,
+                 'Entry #2:',
+                 'Country Name: BPr',
+                 'Prince in USD: K4ZSJ0Q',
+                 'Local Currency: 0p88',
+                 'Price in local currency: hL1Wa',
+                 'USD exchange rate: W9Uw5Ul',
+                 'Date: SKj43n7b',
+                 '-' * 15,
+             ])
+        ]
+    )
+    async def test_raw_data_option_should_trigger_and_display_correctly(
+        self,
+        prices: ProvisionedPricesViewModel,
+        expected_lines: list[str],
+        monkeypatch: MonkeyPatch,
+    ) -> None:
+        expected_text: str = '\n'.join(expected_lines)
+        dummy_input: Callable[[str], str] = self._decoy.mock(func=Callable[[str], str])  # type: ignore
+        mock_stdout: io.StringIO = io.StringIO()
+
+        self._decoy.when(
+            await self._dummy_price_provisioning_controller.provision_prices()
+        ).then_return(Result.ok(prices))
+
+        self._decoy.when(
+            dummy_input('\nChoose an option...\n')
+        ).then_return('1')
+
+        monkeypatch.setattr(builtins, 'input', dummy_input)
+        monkeypatch.setattr(sys, 'stdout', mock_stdout)
+
+        await self._menu.run()
+
+        assert expected_text in mock_stdout.getvalue()
